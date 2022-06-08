b0c8898c7503d0b8d322a6471c60b300aab59ed9
ThomasJose1999 <thomas.jose@phaethon.tech>
2022-06-08 21:21:32 +0530

    contract.sol done

---
+++ contract.sol
@@ -0,0 +1,22 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract transfer{
+    address owner;
+    
+    constructor () public {
+        owner = msg.sender;
+    }
+
+    function sendMoney(address payable addr, uint256 value) public payable {
+        require (value < address(this).balance, "contract doesn't have enough balance");
+        addr.transfer(value);
+    }
+
+    function BuffContract() public payable {
+
+    }
+
+    function balanceOfContract () public view returns(uint256){
+        return address(this).balance;
+    }
+}
